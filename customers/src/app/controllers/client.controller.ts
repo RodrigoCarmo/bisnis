a1901ae3e8cf20b8d4736eb54de5afe17605aec7
rodrigocarmo <roodrigo_5634@hotmail.com>
2023-06-11 18:28:56 -0300

    refact: adaptando para microservicos

--- customers/src/app/controllers/client.controller.ts
+++ customers/src/app/controllers/client.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, HttpCode, Post, UseFilters } from "@nestjs/common";
+import { Controller, UseFilters } from "@nestjs/common";
 import { HttpExceptionFilter } from "src/utils/http-exception.filter";
 import { ClientService } from "../services/client.service";
 import { CreateCustomerDto } from "../dtos/customer.dto";
+import { MessagePattern } from "@nestjs/microservices";
+import { CustomersModel } from "src/domain/models/customers.model";
 
-@Controller("customers")
+@Controller()
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
-  @Post()
-  @HttpCode(201)
   @UseFilters(new HttpExceptionFilter())
-  async create(@Body() createCustomerDto: CreateCustomerDto): Promise<any> {
+  @MessagePattern({ cmd: "create_customer" })
+  async create(
+    createCustomerDto: CreateCustomerDto
+  ): Promise<Partial<CustomersModel>> {
     return await this.clientService.create(createCustomerDto);
   }
 }
--- customers/src/main.ts
+++ customers/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableVersioning({ prefix: "1", type: 0 });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: 3001 },
+    }
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen();
 }
 bootstrap();
