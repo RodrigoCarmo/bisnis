213efd2d5f54611d03bba22901748f499f1420ea
rodrigocarmo <roodrigo_5634@hotmail.com>
2023-06-10 22:37:42 -0300

    feat: adiciona prefixo de versao no path e uso global do validationPipe padrao

--- customers/src/main.ts
+++ customers/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({ prefix: "1", type: 0 });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
